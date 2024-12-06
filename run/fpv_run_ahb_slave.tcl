09b6522a702873b3af2ceae2f61b6f9911d1f2ca
kamalapuram09 <163194328+kamalapuram09@users.noreply.github.com>
2024-12-06 15:10:38 -0800

    Update fpv_run_ahb_slave.tcl

--- run/fpv_run_ahb_slave.tcl
+++ run/fpv_run_ahb_slave.tcl
@@ -2,7 +2,7 @@
 set_fml_appmode FPV
 set design AHB_slave_interface
 
-read_file -top $design -format sverilog -sva  -vcs {../RTL/filelist}
+read_file -top $design -format sverilog -sva  -vcs {-f ../RTL/filelist}
 
 create_clock Hclk -period 100 
 create_reset Hresetn -sense low
