3bd3cd17a986ee97a26fffe6e28cffc944288107
kamalapuram09 <163194328+kamalapuram09@users.noreply.github.com>
2024-12-06 11:36:21 -0800

    Update fpv_run_apb_controller.tcl

--- run/fpv_run_apb_controller.tcl
+++ run/fpv_run_apb_controller.tcl
@@ -1,12 +1,12 @@
 
 # Select AEP as the VC Formal App mode
 set_fml_appmode FPV
-set design APB_FSM_Controller
+set design APB_Controller
 
 set_fml_var fml_aep_unique_name true
 read_file -top $design -format sverilog -sva \
 -aep all -vcs {../RTL/APB_Controller.v +define+INLINE_SVA \
-   ../sva/APB_FSM_Controller_sva.sv}
+   ../sva/APB_Controller_sva.sv}
 #read_waiver_file -elfiles aep.el
 
 # Creating clock and reset signals
